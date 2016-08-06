38423ef23b97bab8a8138060b4f385e75cf1f503
Li Tze Han <Li Tze Han>
2016-08-06 13:14:11 +1200

    Add project files and folders

---
+++ helloPerson.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "blabla";
+document.body.innerHTML = greeter(user);
---
+++ helloPerson.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+var user = "blabla";
+
+document.body.innerHTML = greeter(user); 
